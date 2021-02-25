03dcd9284c15911db756089fc12381a78b855bea
Sean Morrissey <semorrissey17@gmail.com>
2021-02-25 17:05:17 -0500

    readded Line Message API example

--- liff server.js
+++ liff server.js
@@ -1,9 +1,19 @@
+const line = require('@line/bot-sdk');
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 const myLiffId = process.env.MY_LIFF_ID;
 
 require('dotenv').config();
+
+// create LINE SDK config from env variables
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET,
+};
+
+// create LINE SDK client
+const client = new line.Client(config);
 
 app.use(express.static('public'));
 
@@ -14,5 +24,31 @@ app.get('/send-id', function(req, res) {
 app.get("/", function(req, res) {
   res.sendFile(__dirname + "/public/LIFF.html");
 });
+
+// register a webhook handler with middleware
+// about the middleware, please refer to doc
+app.post('/callback', line.middleware(config), (req, res) => {
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
+});
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    // ignore non-text-message event
+    return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
 
 app.listen(port, () => console.log(`app listening on port ${port}!`));
