37f48b2da6485e77812a7921316474174e0cae3a
Sean Morrissey <semorrissey17@gmail.com>
2021-03-01 17:12:48 -0500

    test commit #7
    hopefully this works :)

--- liff server.js
+++ liff server.js
@@ -79,9 +79,11 @@ function handleEvent(event) {
 
         const date = new Date(stationData[0].time.substr(0, 18));
         const messageResponse = "On " + date.toDateString + ", \n " + "the temperature is " + stationData[0].temp + " ℃, \n" + "PM1 concentration is " + stationData[0]["pm1"] + ", \n" + "PM25 concentration is " + stationData[0]["pm25"] + ", \n" + "PM10 concentration is " + stationData[0]["pm10"] + ", \n" + "CO2 concentration is " + stationData[0]["co2"] + ", \n" + "The humidity is " + stationData[0].humid;
-
+        console.log(messageResponse);
         return messageResponse;
       });
+    console.log("I made it! \n");
+    console.log(fetchCall);
     return client.replyMessage(event.replyToken, {
       type: 'text',
       text: fetchCall
