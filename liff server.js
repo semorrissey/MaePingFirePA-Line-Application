0bb2c545c2f9726ec2c21aca189f872ae73b4969
Sean Morrissey <semorrissey17@gmail.com>
2021-03-01 18:18:35 -0500

    test commit #18

--- liff server.js
+++ liff server.js
@@ -40,33 +40,6 @@ app.post('/callback', line.middleware(config), (req, res) => {
     });
 });
 
-async function cuSenseFetch() {
-  let result = await Promise.all([fetch('https://www.cusense.net:8082/api/v1/sensorData/realtime/all', {
-      method: 'POST',
-      headers: {
-        'X-Gravitee-Api-Key': '3d9c7df5-1262-45ad-a311-ff5ae72b4cb8',
-        'Content-Type': 'application/json'
-      },
-      body: '{\"topic\":\"cusensor2/60019440B80B\"}'
-    })
-    .then(response => {
-      let json = response.json();
-      return json;
-    })
-    .then(responseData => {
-      const stationData = responseData["cusensor2/60019440B80B"].data;
-      stationData.name = responseData["cusensor2/60019440B80B"].info["name"];
-      stationData.province = responseData["cusensor2/60019440B80B"].info["province"];
-
-      const date = new Date(stationData[0].time.substr(0, 18));
-      const messageResponse = "On " + date.toDateString() + ", \n" + "The temperature is " + stationData[0].temp + " ℃, \n" + "PM1 concentration is " + stationData[0]["pm1"] + ", \n" + "PM25 concentration is " + stationData[0]["pm25"] + ", \n" + "PM10 concentration is " + stationData[0]["pm10"] + ", \n" + "CO2 concentration is " + stationData[0]["co2"] + ", \n" + "The humidity is " + stationData[0].humid;
-
-      return messageResponse;
-    })
-  ]);
-  return result;
-}
-
 // event handler
 function handleEvent(event) {
   if (event.type !== 'message' || event.message.type !== 'text') {
@@ -86,11 +59,31 @@ function handleEvent(event) {
     });
   } else if (event.message.text.match("CUsense")) {
 
-    const fetchCall = cuSenseFetch();
-    console.log(fetchCall);
+    const fetchCall = await fetch('https://www.cusense.net:8082/api/v1/sensorData/realtime/all', {
+        method: 'POST',
+        headers: {
+          'X-Gravitee-Api-Key': '3d9c7df5-1262-45ad-a311-ff5ae72b4cb8',
+          'Content-Type': 'application/json'
+        },
+        body: '{\"topic\":\"cusensor2/60019440B80B\"}'
+      })
+      .then(response => {
+        let json = response.json();
+        return json;
+      })
+      .then(responseData => {
+        const stationData = responseData["cusensor2/60019440B80B"].data;
+        stationData.name = responseData["cusensor2/60019440B80B"].info["name"];
+        stationData.province = responseData["cusensor2/60019440B80B"].info["province"];
+
+        const date = new Date(stationData[0].time.substr(0, 18));
+        const messageResponse = "On " + date.toDateString() + ", \n" + "The temperature is " + stationData[0].temp + " ℃, \n" + "PM1 concentration is " + stationData[0]["pm1"] + ", \n" + "PM25 concentration is " + stationData[0]["pm25"] + ", \n" + "PM10 concentration is " + stationData[0]["pm10"] + ", \n" + "CO2 concentration is " + stationData[0]["co2"] + ", \n" + "The humidity is " + stationData[0].humid;
+
+        return messageResponse;
+      })
     return client.replyMessage(event.replyToken, {
       type: 'text',
-      text: fetchCall.toLocaleString()
+      text: fetchCall
     });
   } else if (event.message.text.match("Windy")) {
     return client.replyMessage(event.replyToken, {
