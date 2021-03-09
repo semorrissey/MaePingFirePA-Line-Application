4c66242c9174d8ebdc3a97775af12be0195defd7
Sean Morrissey <semorrissey17@gmail.com>
2021-03-08 19:00:44 -0500

    debugging wget

--- liff server.js
+++ liff server.js
@@ -66,7 +66,23 @@ app.post('/callback', line.middleware(config), (req, res) => {
 });
 
 function csvDownload() {
-  wget('https://raw.github.com/angleman/wgetjs/master/angleman.png');
+  wget({
+      url: 'https://raw.github.com/angleman/wgetjs/master/package.json',
+      dest: '/tmp/', // destination path or path with filenname, default is ./
+      timeout: 2000 // duration to wait for request fulfillment in milliseconds, default is 2 seconds
+    },
+    function(error, response, body) {
+      if (error) {
+        console.log('--- error:');
+        console.log(error); // error encountered
+      } else {
+        console.log('--- headers:');
+        console.log(response.headers); // response headers
+        console.log('--- body:');
+        console.log(body); // content of package
+      }
+    }
+  );
   return true;
 }
 
