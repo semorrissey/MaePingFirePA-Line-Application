3767d2bf530ffd7ed90255ee46feedac96918465
Sean Morrissey <semorrissey17@gmail.com>
2021-03-11 17:35:35 -0500

    debugging mongodb payload

--- liff server.js
+++ liff server.js
@@ -22,12 +22,13 @@ const config = {
 const client = new line.Client(config);
 
 app.use(express.static('public'));
-app.use(express.json({
-  limit: '25mb'
+app.use(bodyParser.json({
+  limit: '50mb'
 }));
-/*app.use(express.urlencoded({
-  limit: '25mb'
-}));*/
+app.use(bodyParser.urlencoded({
+  limit: '50mb',
+  extended: true
+}));
 
 
 app.get('/send-id', function(req, res) {
