9845b8e9d19944b6f201be86c9d1f12bdf6a9148
Sean Morrissey <semorrissey17@gmail.com>
2021-03-15 16:47:25 -0400

    updated changes to design and cusense webpage

--- public/cusense.js
+++ public/cusense.js
@@ -1,15 +1,27 @@
  async function getData() {
-   var dataString = "";
+   document.getElementById("databox").innerHTML = "";
+   document.getElementById("progBar").style.width = "0%";
+   await sleep(2000);
+   document.getElementById("progBar").style.width = "25%";
+
    fetch("/recieve", {
        method: "GET"
      })
      .then(function(response) {
+       document.getElementById("progBar").style.width = "50%";
        return response.json();
      })
      .then(function(data) {
+       document.getElementById("progBar").style.width = "75%";
        console.log(data);
-       var formattedString = data.split(",").join("<br>");
+       var formattedString = data.split(",").join("<br>").split("Data changes every hour!").join("<br> <br>");
+
        document.getElementById("databox").innerHTML = formattedString;
+       document.getElementById("progBar").style.width = "100%";
      });
 
+ }
+
+ function sleep(ms) {
+   return new Promise(resolve => setTimeout(resolve, ms));
  }
